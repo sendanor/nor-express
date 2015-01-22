368f936ad95e57f6f66509c87b9c7d8118276f9c
Jaakko-Heikki Heusala <jheusala@iki.fi>
2015-01-23 00:44:00 +0200

    Fixed issue with object as an error

--- src/error_handlers/debuglog.js
+++ src/error_handlers/debuglog.js
@@ -1,5 +1,6 @@
 /* Sends error in debug log. If `opts.next` is true, then this error handler continues on next handler always. */
 "use strict";
+var is = require('nor-is');
 var debug = require('nor-debug');
 module.exports = function error_handler_debuglog(opts) {
 	//debug.log('here');
@@ -10,7 +11,7 @@ module.exports = function error_handler_debuglog(opts) {
 		if(req.id) {
 			prefix += '[' + req.id + '] ';
 		}
-		debug.error( ('-- Exception --\n' + (err.stack || err) + '\n----').split('\n').join('\n' + prefix) + '\n' );
+		debug.error( ('-- Exception --\n' + (err.stack || (is.obj(err) ? JSON.stringify(err, null, 2) : err)) + '\n----').split('\n').join('\n' + prefix) + '\n' );
 		if(go_next) {
 			debug.assert(next).is('function');
 			next(err);
